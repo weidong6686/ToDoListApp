e03238523f8f9fe720c0c9042f7970b937f50a72
weidong6686 <weidong6686@gmail.com>
2020-05-22 22:51:01 -0700

    Added new validation feature to the completion of tasks

--- ToDoList/ToDoDetailsViewController.swift
+++ ToDoList/ToDoDetailsViewController.swift
@@ -61,6 +61,32 @@ class ToDoDetailsViewController: UIViewController {
     
     @IBAction func taskDidComplete(_ sender: Any) {
         
+        let alert = UIAlertController(title: "Confirm", message: "Are you sure you would like to complete this task?", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            
+            alert.dismiss(animated: true, completion: nil)
+            
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            
+            self.completeTask(alert: alert)
+            
+        }
+        
+        alert.addAction(cancelAction)
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+
+        
+    }
+    
+    func completeTask(alert: UIAlertController) {
+        
         toDoItem.isComplete = true
 
         delegate?.update(task: toDoItem, index: toDoIndex)
@@ -69,5 +95,4 @@ class ToDoDetailsViewController: UIViewController {
         
     }
     
-
 }
